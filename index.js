d72b2d711dab54b7a6bbffd9e0bc9b777e1c20c8
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-11 16:57:51 -0700

    commit tree d70635eacd3e094246ff2a60cd40757b4d0344ec

---
+++ index.js
@@ -0,0 +1,31 @@
+// // const divide = (parameter1, parameter2) => parameter1 / parameter2;
+// // divide(2000, 100);
+// // console.log(divide);
+// function divide() {
+//   return 2000 / 100;
+// }
+
+// const hello = (x) => x + 1;
+
+// const square = (x) => x * x;
+
+// const add = (x, y) => x + y;
+// add(1, 2);
+
+// const divide = function (a = "2000", b = "100") {
+//   return a / b;
+// };
+// divide();
+
+// const square = (num1) => num1 * num1;
+
+// const add = (num1, num2) => num1 + num2;
+
+const divide = function (num1 = "2000", num2 = "100") {
+  return num1 / num2;
+};
+console.log(divide());
+
+const square = (n) => n * n;
+
+const add = (n, m) => n + m;
